cbb7973467853de4d0ed787ac043d8bf6351037d
Caroline Quigg <caquigg@microsoft.com>
2022-06-26 14:51:30 -0700

    simple bot framework

---
+++ FoodTruckBot/FoodTruckBot/Bots/FoodTruckBot.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+
+using FoodTruckBot.States;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoodTruckBot.Bots
+{
+    public class FoodTruckBot : ActivityHandler
+    {
+        // Messages sent to the user.
+        private const string WelcomeMessage = "Hello there! Welcome to FoodTruck Bot."+
+                                              "Let me help you find some tasty places to eat near you. ";
+
+        private const string PatternMessage = "Try it now, type 'hi'";
+
+        private readonly BotState _userState;
+
+        // Initializes a new instance of the "WelcomeUserBot" class.
+        public FoodTruckBot(UserState userState)
+        {
+            _userState = userState;
+        }
+
+        // Greet when users are added to the conversation.
+        // Note that all channels do not send the conversation update activity.
+        // If you find that this bot works in the emulator, but does not in
+        // another channel the reason is most likely that the channel does not
+        // send this activity.
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(WelcomeMessage, cancellationToken: cancellationToken);
+                    await turnContext.SendActivityAsync(PatternMessage, cancellationToken: cancellationToken);
+                }
+            }
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeUserStateAccessor = _userState.CreateProperty<WelcomeUserState>(nameof(WelcomeUserState));
+            var didBotWelcomeUser = await welcomeUserStateAccessor.GetAsync(turnContext, () => new WelcomeUserState(), cancellationToken);
+
+            if (didBotWelcomeUser.DidBotWelcomeUser == false)
+            {
+                didBotWelcomeUser.DidBotWelcomeUser = true;
+
+                // the channel should sends the user name in the 'From' object
+                var userName = turnContext.Activity.From.Name;
+
+                await turnContext.SendActivityAsync("I see this is your first time using me.", cancellationToken: cancellationToken);
+                await turnContext.SendActivityAsync("Go ahead and enter your latitude & longitude and I'll get you some recommendations", cancellationToken: cancellationToken);
+            }
+            else
+            {
+                var text = turnContext.Activity.Text.ToLowerInvariant();
+                switch (text)
+                {
+                    case "hello":
+                    case "hi":
+                        await turnContext.SendActivityAsync($"You said {text}.", cancellationToken: cancellationToken);
+                        break;
+                    case "intro":
+                    case "help":
+                        await SendIntroCardAsync(turnContext, cancellationToken);
+                        break;
+                    default:
+                        await turnContext.SendActivityAsync($"Your location is {text}", cancellationToken: cancellationToken);
+                        break;
+                }
+            }
+
+            // Save any state changes.
+            await _userState.SaveChangesAsync(turnContext, cancellationToken: cancellationToken);
+        }
+
+        private static async Task SendIntroCardAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var card = new HeroCard
+            {
+                Title = "Welcome to Bot Framework!",
+                Text = "Welcome to Welcome Users bot sample! This Introduction card "+
+                         "is a great way to introduce your Bot to the user and suggest "+
+                         "some things to get them started. We use this opportunity to "+
+                         "recommend a few next steps for learning more creating and deploying bots.",
+                Images = new List<CardImage>() { new CardImage("https://aka.ms/bf-welcome-card-image") },
+                Buttons = new List<CardAction>()
+                {
+                    new CardAction(ActionTypes.OpenUrl, "Get an overview", null, "Get an overview", "Get an overview", "https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0"),
+                    new CardAction(ActionTypes.OpenUrl, "Ask a question", null, "Ask a question", "Ask a question", "https://stackoverflow.com/questions/tagged/botframework"),
+                    new CardAction(ActionTypes.OpenUrl, "Learn how to deploy", null, "Learn how to deploy", "Learn how to deploy", "https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-howto-deploy-azure?view=azure-bot-service-4.0"),
+                }
+            };
+
+            var response = MessageFactory.Attachment(card.ToAttachment());
+            await turnContext.SendActivityAsync(response, cancellationToken);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# sf-foodtruck-chatbot
-Chat with the MS Teams bot to find your favorite SF food trucks
+# SF FoodTruck Bot
+Chat with the food truck bot to find your favorite SF food trucks. This is my first time working with Azure's Bot Framework, so thought it would be a fun way to learn something new.
+
+#### Development Prerequisites & Resources
+- [ASP.NET Core Runtime 3.1](https://dotnet.microsoft.com/en-us/download/dotnet/3.1)
+- [Bot Framework Emulator](https://github.com/microsoft/BotFramework-Emulator/blob/master/README.md)
+- [Azure Bot Framework SDK](https://docs.microsoft.com/en-us/azure/bot-service/index-bf-sdk?view=azure-bot-service-4.0)
+
+
+#### Local Setup
+
+Download and open the solution and make sure it builds. You will need .NET Core 3.1 and the Bot Framework Emulator downloaded and installed as well. Once you verify everything builds correctly and all your dependencies are installed, time to get the bot up and running!
+
+1. Start the project _without debugging_ this will start up the application and deploy it to localhost, on port 3798 by default. You can now connect to the bot using the emulator.
+2. Open the bot emulator and select 'Open Bot', enter the bot's url `http://localhost:3978/api/messages` and hit 'Connect'.
+3. The emulator will then open up a message window the the bot and you can now send and receive messages from the bot.
+
